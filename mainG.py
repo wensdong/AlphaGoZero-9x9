116d71012ba7b497bb5661d04527f7db7ec6f30e
wensdong <wensdong@gmail.com>
2018-04-24 17:03:06 +0100

    Add files via upload

---
+++ mainG.py
@@ -0,0 +1,38 @@
+from Coach import Coach
+#from othello.OthelloGame import OthelloGame as Game
+#from othello.pytorch.NNet import NNetWrapper as nn
+from utils import *
+
+from go.GoGame import GoGame
+from go.GoPlayers import *
+from go.pytorch.NNet import NNetWrapper as nn
+
+args = dotdict({
+    'numIters': 10,
+    'numEps': 10,
+    'tempThreshold': 15,
+    'updateThreshold': 0.6,
+    'maxlenOfQueue': 200000,
+    'numMCTSSims': 10,
+    'arenaCompare': 10,
+    'cpuct': 1,
+
+    'checkpoint': './temp/',
+    'load_model': False,
+    'load_folder_file': ('/dev/models/8x100x50','best.pth.tar'),
+    'numItersForTrainExamplesHistory': 20,
+
+})
+
+if __name__=="__main__":
+    g = GoGame(6)
+    nnet = nn(g)
+
+    if args.load_model:
+        nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
+
+    c = Coach(g, nnet, args)
+    if args.load_model:
+        print("Load trainExamples from file")
+        c.loadTrainExamples()
+    c.learn()
