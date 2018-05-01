9272e83f0711cda2ec26cd951ae4a5d9961b9933
wensdong <wensdong@gmail.com>
2018-05-01 19:02:23 +0100

    Add files via upload

--- pit.py
+++ pit.py
@@ -1,8 +1,8 @@
 import Arena
 from MCTS import MCTS
-from tictactoe.TicTacToeGame import TicTacToeGame, display
-from tictactoe.TicTacToePlayers import *
-from tictactoe.keras.NNet import NNetWrapper as NNet
+from go.GoGame import GoGame, display
+from go.GoPlayers import *
+from go.pytorch.NNet import NNetWrapper as NNet
 
 import numpy as np
 from utils import *
@@ -12,26 +12,26 @@ use this script to play any two agents against each other, or play manually with
 any agent.
 """
 
-g = TicTacToeGame()
+g = GoGame(5)
 
 # all players
 rp = RandomPlayer(g).play
 #gp = GreedyOthelloPlayer(g).play
-hp = HumanTicTacToePlayer(g).play
+hp = HumanGoPlayer(g).play
 
 # nnet players
 n1 = NNet(g)
-n1.load_checkpoint('./pretrained_models/tictactoe/keras','best-25eps-25sim-10epch.pth.tar')
-args1 = dotdict({'numMCTSSims': 10, 'cpuct':1.0})
+n1.load_checkpoint('./temp','temp.pth.tar')
+args1 = dotdict({'numMCTSSims': 15, 'cpuct':1.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
 
 
 n2 = NNet(g)
-n2.load_checkpoint('/temp/','temp.pth.tar')
-args2 = dotdict({'numMCTSSims': 25, 'cpuct':1.0})
+n2.load_checkpoint('./temp/','temp.pth.tar')
+args2 = dotdict({'numMCTSSims': 15, 'cpuct':1.0})
 mcts2 = MCTS(g, n2, args2)
 n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
 
 arena = Arena.Arena(n1p, n2p, g, display=display)
-print(arena.playGames(20, verbose=True))
+print(arena.playGames(10, verbose=True))
