b476f75586f6b4ff34516adfe633a812fbdaad87
wensdong <wensdong@gmail.com>
2018-05-01 19:00:31 +0100

    update GoLogic
    Working on ubuntu

--- go/GoLogic.py
+++ go/GoLogic.py
@@ -56,12 +56,14 @@ class Board():
         for y in range(self.n):
             for x in range(self.n):
                 if self[x][y]==0:
-                    if not self.is_surrounded((x, y), color) and not self.is_2eyes((x, y), color):
-                        """must empty space and not suicide"""
-                        moves.append((x,y))
-                    elif self.is_surrounded((x,y), color) and self.is_kill((x,y), color) and \
-                        not self.is_ko((x,y), color):
-                        moves.append((x,y))
+                    if not self.is_surrounded((x, y), color):
+                        if not self.is_2eyes((x, y), color):
+                            """must empty space and not suicide"""
+                            moves.append((x,y))
+                    else:
+                        if self.is_surrounded((x,y), color):
+                            if self.is_kill((x,y), color) and not self.is_ko((x,y), color):
+                                moves.append((x,y))
         return list(moves)
 
     def has_legal_moves(self, color):
@@ -82,7 +84,7 @@ class Board():
                     self[u][v] = 0
                     return str(True), dead_pieces
         self[u][v] = 0
-        return 
+        return
 
 
     def perform_kill(self, point, color):
@@ -164,33 +166,33 @@ class Board():
             count +=1
 
         return collection
-    
-    
+
+
     def is_2eyes(self, point, color):
         """for a group of connected pieces, if there are 2 empty point inside, then the group is live group"""
         conn = []
         nei = self.get_neighbors((point), color)
-        
+
         conn_dict = {}
-        
+
         if len(nei) > 1:
             conn = self.connected(nei[0], color)
             if all((x, y) in conn for (x, y) in nei):
-                nei_of_conn = [self.get_neighbors_any((x,y), color) for x, y in conn] 
+                nei_of_conn = [self.get_neighbors_any((x,y), color) for x, y in conn]
                 nei_of_conn_flat = [item for sublist in nei_of_conn for item in sublist]
                 nei_of_conn_dict = {k:nei_of_conn_flat.count(k) for k in nei_of_conn_flat}
                 """find all points, which have 4 its own neighbors at inside board position, 3 at edge and 2 at corner"""
                 inside_points = {k: v for k, v in nei_of_conn_dict.items() if v==4}
                 edge_points = {(x,y): v for (x,y), v in nei_of_conn_dict.items() if v == 3 and ((x==self.n - 1 and                             not y== self.n-1) or (y == self.n -1 and not x == self.n-1) or                             (x ==0 and not y ==0) or (y ==0 and not x ==0))}
-                
+
                 corner_points = {(x,y): v for (x,y), v in nei_of_conn_dict.items() if v == 2 and                                  ((x ==self.n-1 or x ==0) and (y == self.n-1 or y == 0))}
-               
+
                 ok_points = {**corner_points, **edge_points, **inside_points}
                 """ covert dict to list for key only"""
                 ok_points = [key for key, value in ok_points.items()]
                 """filter one of 2 eyes"""
                 eyes = [(x, y) for (x, y) in ok_points if self[x][y] == 0]
-                
+
                 if len(eyes) == 2 and point in eyes:
                     return True
 
@@ -212,36 +214,32 @@ class Board():
                 return True
             else:
                 return False
-            
+
 
     def is_ko(self, point, color):
         """test whether the move is ko, which cause board status return to previous status,
         before your opponent's last move"""
         x, y = point
         assert(self[x][y] == 0)
-        
+
         if self.history_ and self.history:
             if self.history_[-1] is not None:
                 if self.is_kill((point), color):
                     _, dead_pieces = self.is_kill((point), color)
-                    
+
                     if self.history_[-1][-1] == point and dead_pieces[-1] == self.history[-1]:
                         return True
-    
+
     def execute_move(self, move, color):
         """Perform the given move on the board; remove opponet pieces if kill (1=white,-1=black)
         """
         x, y= move
-        #if (x, y) in self.get_legal_moves(color):
-        self[x][y] = color
-        self.history.append((x, y))
-        dead_pieces = self.perform_kill(move, color)
-        if len(dead_pieces) > 0:
-            self.history_.append(dead_pieces)
-        else:
-            self.history_.append(None)
-        return self.history, self.history_
-
-
-
-
+        if (x, y) in self.get_legal_moves(color):
+            self[x][y] = color
+            self.history.append((x, y))
+            dead_pieces = self.perform_kill(move, color)
+            if len(dead_pieces) > 0:
+                self.history_.append(dead_pieces)
+            else:
+                self.history_.append(None)
+            return self.history, self.history_
