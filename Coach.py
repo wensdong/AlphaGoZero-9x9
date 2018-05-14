426dd0df4cd4e27991bd1314f38851ac9d2dae3a
wensdong <wensdong@gmail.com>
2018-05-14 18:43:00 +0800

    Update Coach.py

--- Coach.py
+++ Coach.py
@@ -56,7 +56,7 @@ class Coach():
             action = np.random.choice(len(pi), p=pi)
             board, self.curPlayer = self.game.getNextState(board, self.curPlayer, action)
 
-            r = self.game.getGameEnded(board, self.curPlayer, action)
+            r = self.game.getGameEnded(board, self.curPlayer)
 
             if r!=0:
                 return [(x[0],x[2],r*((-1)**(x[1]!=self.curPlayer))) for x in trainExamples]
