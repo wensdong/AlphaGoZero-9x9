d92bd89e8febfac5b19b68140eec1b3aeecad861
wensdong <wensdong@gmail.com>
2018-05-23 07:51:00 +0800

    GoNNet
    add padding to allow 3x3 board size

--- go/pytorch/GoNNet.py
+++ go/pytorch/GoNNet.py
@@ -20,7 +20,7 @@ class GoNNet(nn.Module):
         super(GoNNet, self).__init__()
         self.conv1 = nn.Conv2d(1, args.num_channels, 3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(args.num_channels, args.num_channels, 3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(args.num_channels, args.num_channels, 3, stride=1)
+        self.conv3 = nn.Conv2d(args.num_channels, args.num_channels, 3, stride=1, padding =1)
         self.conv4 = nn.Conv2d(args.num_channels, args.num_channels, 3, stride=1)
 
         self.bn1 = nn.BatchNorm2d(args.num_channels)
@@ -45,7 +45,7 @@ class GoNNet(nn.Module):
         s = F.relu(self.bn2(self.conv2(s)))                          # batch_size x num_channels x board_x x board_y
         s = F.relu(self.bn3(self.conv3(s)))                          # batch_size x num_channels x (board_x-2) x (board_y-2)
         s = F.relu(self.bn4(self.conv4(s)))                          # batch_size x num_channels x (board_x-4) x (board_y-4)
-        s = s.view(-1, self.args.num_channels*(self.board_x-4)*(self.board_y-4))
+        s = s.view(-1, self.args.num_channels*(self.board_x-2)*(self.board_y-2))
 
         s = F.dropout(F.relu(self.fc_bn1(self.fc1(s))), p=self.args.dropout, training=self.training)  # batch_size x 1024
         s = F.dropout(F.relu(self.fc_bn2(self.fc2(s))), p=self.args.dropout, training=self.training)  # batch_size x 512
